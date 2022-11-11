eb3afa4f669e30693d10a6f49fd08e337385dfcf
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-11-11 09:27:20 +0000

    intial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const BASE_URL = "https://striveschool-api.herokuapp.com/api/movies"
+const headers = new Headers({
+"Content-Type": "application/json",
+Authorization:
+"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjQyMmQ0YmUzZDAwMTU4NDVmZWYiLCJpYXQiOjE2NjgxMDk5OTcsImV4cCI6MTY2OTMxOTU5N30.vdaqA6iDLq3NY1CpqygeUpueFbYiffQOo2ZFwNKiWAc" ,
+})
+
+const getCats = (callback) => {
+    fetch ()
+}
